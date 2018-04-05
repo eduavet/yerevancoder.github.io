d50254f036249807bed39301b2dd6aa97458d012
Edgar Aroutiounian <edgar.factorial@gmail.com>
2018-04-05 11:08:32 -0700

    Dedicate effort to mobile bar with nav

--- src/components/mobile-bar.js
+++ src/components/mobile-bar.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Link from 'gatsby-link';
 
+import { ROUTES, SPACER_10_H } from '../utils/constants';
+
 const yc = (
   <Link className={'BlogTable__MobileBarBanner'} to={'/'}>
     yerevancoder
@@ -11,5 +13,17 @@ export default (
   <div className={'BlogTable__MobileBar'}>
     <img className={'BlogTable__MobileBarSiteLogo'} src={'/mobile-bar-logo.png'} />
     {yc}
+    <div className={'BlogTable__MobileBarNavBox PlainFlexRow'}>
+      <div className={'PlainFlexColumn BlogTable__MobileBarNavBoxElement'}>
+        <Link to={ROUTES.AVAILABLE_FOR_WORK}>freelancers</Link>
+        {SPACER_10_H}
+        <Link to={ROUTES.JOBS_TABLE}>hiring board</Link>
+      </div>
+      <div className={'PlainFlexColumn'}>
+        <a href={'https://forum.yerevancoder.com/categories'}>forums</a>
+        {SPACER_10_H}
+        <a href={'https://iteratehackerspace.com'}>iterate</a>
+      </div>
+    </div>
   </div>
 );
