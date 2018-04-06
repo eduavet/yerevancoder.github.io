421bb9497e9c80f162613bd2a5c0bc0fff66eda6
Edgar Aroutiounian <edgar.factorial@gmail.com>
2018-04-06 09:15:14 -0700

    Remove typography from built step

--- gatsby-config.js
+++ gatsby-config.js
@@ -61,11 +61,5 @@ module.exports = {
     `gatsby-plugin-feed`,
     `gatsby-plugin-offline`,
     `gatsby-plugin-react-helmet`,
-    {
-      resolve: 'gatsby-plugin-typography',
-      options: {
-        pathToConfigModule: 'src/utils/typography',
-      },
-    },
   ],
 };
