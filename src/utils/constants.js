9a1d5050c013bbf70c668fba6e61c23222257888
Edgar Aroutiounian <edgar.factorial@gmail.com>
2018-04-09 11:00:16 -0700

    Add email regex and default new job email subject

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -229,4 +229,6 @@ export const DID_YOU_KNOW = (
   </span>
 );
 
-export const EMAIL_REGEX = new RegExp('', 'ig');
+export const EMAIL_REGEX = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+export const EMAIL_NEW_JOB_SUBJECT = encodeURI(`I saw your job posted on yerevancoder.com`);
